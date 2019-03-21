b77a8d4b862dafab5bd761cce9d52611e90407b7
MoltenAqua <48008733+MoltenAqua@users.noreply.github.com>
2019-03-21 21:52:44 +0000

    Task 5 - Highlighting Moves

--- board.java
+++ board.java
@@ -13,36 +13,24 @@ import java.lang.Object;
 
 public class board 
 {
-
-        public static square prevSq;
-        public static String currentPlayer = "WHITE";
-        private static  JFrame window = new JFrame();
-        private static JPanel panel = new JPanel();
-        private int o = 0;
-        private int type = 1;
-        public static square[][] button = new square[8][8];
-        JButton press = new JButton();
+        //instance variables for board
+        public static square prevSq; //remembers the previously clicked square 
+        public static String currentPlayer = "WHITE"; // tracks the current player - white starts
+        private static  JFrame window = new JFrame(); //creates the window for the game
+        private static JPanel panel = new JPanel(); // creates the panel which is put onto the window
+        public static square[][] button = new square[8][8]; //creates an array of buttons (64)
+   
     
         public static void main(String[] args)
         {
 
-        
-            
-            
-
-            
-            panel.setLayout(new GridLayout(8 ,8, 0, 0));
-            
-        
-            
-
-            
+            panel.setLayout(new GridLayout(8 ,8, 0, 0)); //creates a swing gridlayout for an 8*8 grid
 
             String str = "btn";
             int colour = 0;
             
 
-
+            //loops through 8 rows and 8 columns
             for(int i = 0; i<8; i++)
             {
 
@@ -51,63 +39,51 @@ public class board
                     {
                         
                        
-                       // button[i][j].setRow(i);
-                        //button[i][j].setCol(j);
-                        if(colour==0) //black
+                       
+                        if(colour==0) //if the square needs to be black
                         {
-                         //   button[i][j].setIcon(new ImageIcon("empty2.png"));
-                            
-                            
-                            button[i][j] = new square(str+i+j, i, j, "BLACK");
-                        
-                            panel.add(button[i][j]);
 
-                            colour = 1;
+                            button[i][j] = new square(str+i+j, i, j, "BLACK");//creates a button
+                        
+                            
+
+                            colour = 1; //sets the colour to one, next loop goes to the else statement
 
                         }
-                        else //white
+
+                        else //if the square needs to be white
                         {
-                            
-                          //  button[i][j].setIcon(new ImageIcon("empty.png"));
-                            
-                            
-                            
+ 
+                            //sets the starting point for the red side, they need to have red pieces
                             if(i==0 || i==1 || i==2)
                                 {
-                                    button[i][j] = new square(str+i+j, i, j, "RED");
-                                       // button[i][j].setIcon(new ImageIcon("red.png"));
-                                        
-                                    
+                                    button[i][j] = new square(str+i+j, i, j, "RED"); //creates a red piece 
+                        
                                 }
                             
-                            
-                                else if(i==5 || i==6 || i==7)
+                            // sets the starting point for the white side, they need to have white pieces
+                            else if(i==5 || i==6 || i==7)
                                 {
-                                    button[i][j] = new square(str+i+j, i, j, "WHITE");
-                              //      button[i][j].setIcon(new ImageIcon("white.png"));
-                                    
+                                    button[i][j] = new square(str+i+j, i, j, "WHITE"); //creates a white piece
+                           
                                 }
-                                else
+                            //the remaining pieces are blank
+                            else
                                 {
-                                    button[i][j] = new square(str+i+j, i, j, "NONE");
+                                    button[i][j] = new square(str+i+j, i, j, "NONE"); //creates a blank square
 
 
                                 }    
                             
-                            button[i][j].addActionListener(button[i][j]);                         
+                            button[i][j].addActionListener(button[i][j]);  //implements an action listener on the created button                        
     
-                                            
-                            
-                            panel.add(button[i][j]);
-                            
-                                
-                            
-                            colour = 0;
+                            colour = 0; //switches the colour
                             
                         }
+                        panel.add(button[i][j]);//adds the button to the panel
 
                     }
-
+                    // once it is a new row, this switches the first square colour around
                     if(colour == 0)
                     {
                         colour = 1;
@@ -119,30 +95,64 @@ public class board
 
             }
 
-
-          
-
-            
-            window.pack();
+            window.pack(); 
             
 
-            window.setSize(800,800);
+            window.setSize(800,800); //sets the window size to 800 by 800 pixels
         
         
-            panel.setVisible(true);
-            window.add(panel);
+            panel.setVisible(true); //makes the panel visible to the player(s)
+            window.add(panel); //adds the panel to the window
             
         
-            window.setVisible(true);
+            window.setVisible(true); //makes the window visible to the player(s)
             System.out.println("Hello World!");
-            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //makes the program exit once the window has been closed
         }
 
         public void showRow(square thisSquare)
         {
             thisSquare.getRow();
         }
+        //finds all the possible legal moves for the player and highlights them in yellow
+        public static void showPossible()
+        {
+            // goes through all of the button
+            for(int k = 0; k<=7; k++)
+            {          
+                for(int l=0; l<=7; l++)
+                {
 
-        
- 
+                    //checks if any of the buttons can be moved to legally
+                    if(prevSq.canMoveTo(button[k][l]))
+                    {
+                        
+                       // System.out.println("canMoveTo is true at: " + k + " and " + l); 
+                        
+
+                        button[k][l].setImage("YELLOW"); //sets the legal destination squares to yellow
+                    }
+                }
+            }
+            
+        }
+        //Checks through the buttons for any yellow squares and sets them to blank
+        public static void clearBoard()
+        {
+            //runs through all of the buttons
+            for(int k = 0; k<=7; k++)
+            {          
+                for(int l=0; l<=7; l++)
+                {
+                    //checks if the image is yellow
+                    if(button[k][l].getImage() == "YELLOW")
+                    {
+                        //sets the icon to a blank
+                        button[k][l].setImage("NONE");
+                    }
+                }
+            }
+            
+        }
+
 }
--- square.java
+++ square.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.*;
 import java.lang.Object;
 
-public class square extends JButton implements ActionListener
+public class square extends JButton implements ActionListener // creates a subclass of JButton
 {
 
     private int thisRow;
@@ -14,45 +14,66 @@ public class square extends JButton implements ActionListener
     private String pType;
     public void main(String[] args)
     {
-        System.out.println("hi");
-        
-        
-       
+        System.out.println("hi"); 
         
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-                e.getActionCommand();    
-              //  System.out.println(getRow());
-                //System.out.println(getCol());
-               // moveTo(this);
-               if((pType == "RED" || pType == "WHITE") && pType == board.currentPlayer)
+    public void actionPerformed(ActionEvent e) {  //When the ActionListener detects a mouse click
+                e.getActionCommand();    //does nothing 
+              //if statement checks if the piece is red or white and if the type is the same as the player whose turn it is
+              // ensures the white can only click on white pieces and red can only select red pieces
+               if((pType == "RED" || pType == "WHITE") && pType == board.currentPlayer) 
                {
-                
+                //first click
                  board.prevSq = this; //remember previously clicked square
+                 board.clearBoard(); //removes possible moves marking
+                 board.showPossible(); //shows the available moves to the player
 
                }
-               else if(pType == "YELLOW")
+               else if(pType == "YELLOW") //if it is an allowed move
                {
-
-                    board.prevSq.moveTo(this);  
-                    
-                    if (board.currentPlayer == "WHITE")
+                    //second click on allowed destination square
+                    board.prevSq.moveTo(this);  //runs the move
+                    board.clearBoard();//clears the board of the yellow squares
+                    if (board.currentPlayer == "WHITE") //If it is the white player's turn
                     {
                         board.currentPlayer = "RED";
                     }
-                    else
+                    else //if it is the red player's turn
                     {
                         board.currentPlayer = "WHITE";
                     }
-        
+                
+               
+               
+                     //implement move on previous square to current square
+                    
                }
 
             }
 
-    
+    //checks if the piece can move to the desired square
 
+    public boolean canMoveTo(square testSq)
+    {
+        boolean canGo = false;
+        
+        //red pieces can move down one row, white pieces can move up one row, and one column either way
+        if(this.pType == "RED" && testSq.getImage() == "NONE" && testSq.thisRow == (this.thisRow + 1) && (testSq.col == (this.col + 1) || testSq.col == (this.col - 1)))
+            {
+                canGo = true;
+            }
+        
+        else if(this.pType == "WHITE" && testSq.getImage() == "NONE" && testSq.thisRow == (this.thisRow - 1) && (testSq.col == (this.col + 1) || testSq.col == (this.col - 1)))
+            {
+                canGo = true;
+            }
+        return canGo;
+
+        
+    }
+    //square constructor
     public square(String name, int jRow, int jCol, String image)
     {
         super(name); //invoke the superclass constructor - JButton constructor
@@ -65,41 +86,41 @@ public class square extends JButton implements ActionListener
     }
 
      
-
+    //gets the row for the square
     public int getRow()
     {
         return thisRow;
     }
-
+    //gets the current image of the square
     public String getImage()
     {
         return pType;
     }
-
+    //gets the column for the square
     public int getCol()
     {
         return col;
     }
-
+    //stores the value of the row of the square
     public void setRow(int rowSet)
     {
         thisRow = rowSet;
     }
-
+    //stores the value of the column of the square
     public void setCol(int colSet)
     {
         col = colSet;
     }
-
+    // sets the image of the desired square and clears the place the piece was on
+    //simulates the movement of one piece to another
     public void moveTo(square destSq)
     {
-        //System.out.println(destSq.getIcon());
         
         destSq.setImage(pType);
         setImage("NONE");
     
     }
-
+    //alters the icon depending on the parameter 
     public void setImage(String colour)
     {
         pType = colour;
